533c33b51ea21e6414e3a44a2dd00ea4ee9257bd
si0005hp <si0005hp@gmail.com>
2023-02-05 15:53:55 +0900

    quickstart java

---
+++ quickstart/java/src/main/java/com/si0005hp/spark/quickstart/App.java
@@ -0,0 +1,18 @@
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.api.java.function.FilterFunction;
+
+public class App {
+  public static void main(String[] args) {
+    String logFile = "/opt/spark/README.md"; // Should be some file on your system
+    SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+    long numAs = logData.filter((FilterFunction<String>)s -> s.contains("a")).count();
+    long numBs = logData.filter((FilterFunction<String>)s -> s.contains("b")).count();
+
+    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+    spark.stop();
+  }
+}
